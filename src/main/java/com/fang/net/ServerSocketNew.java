633c43d38baf9e7a9c7232ee73ccd4e02f84b1c4
fangzhigang <fangzhigang2008@163.com>
2016-10-31 11:24:56 +0800

    test java code

---
+++ src/main/java/com/fang/net/ServerSocketNew.java
@@ -0,0 +1,38 @@
+package com.fang.net;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSocketNew {
+	private int port=8000;
+	private ServerSocket serverSocket;
+	
+	public ServerSocketNew () throws IOException{
+		serverSocket = new ServerSocket(port,3);
+		System.out.println("server is startUp");
+	}
+	
+	public void service(){
+		while(true){
+			Socket socket = null;
+			try{
+				socket = serverSocket.accept();
+				System.out.println("New connection accepted"+socket.getInetAddress()+":"+socket.getPort());
+			}catch(IOException e){
+				e.printStackTrace();
+			}finally{
+				try{
+					if(socket !=null)socket.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	public static void main(String[] args) throws InterruptedException, IOException {
+		ServerSocketNew server  = new ServerSocketNew();
+		Thread.sleep(6000);
+		server.service();
+	}
+}
---
+++ src/main/java/com/fang/net2_4/Receiver.java
@@ -0,0 +1,74 @@
+package com.fang.net2_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Receiver {
+	private int port = 8000;
+	private  ServerSocket serverSocket ;
+	
+	private static int stopWay = 1;
+	private final int NUTURAL_STOP = 1;
+	private final int SUDDEN_STOP = 2;
+	private final int SOCKET_STOP = 3;
+	private final int INPUT_STOP = 4;
+	private final int SERVERSOCKET_STOP = 5;
+	
+	
+	public Receiver() throws IOException{
+		serverSocket = new ServerSocket(port);
+		System.out.println("服务器已经启动");
+	}
+	
+	private BufferedReader getReader(Socket socket) throws IOException{
+		InputStream socketIn = socket.getInputStream();
+		return new BufferedReader(new InputStreamReader(socketIn));
+	}
+	public void receive() throws IOException, InterruptedException{
+		Socket socket = null;
+		System.out.println("start receive1");
+		socket = serverSocket.accept();
+		System.out.println("start receive2");
+		BufferedReader br = getReader(socket);
+		System.out.println("start receive3");
+		for(int i=0;i<20;i++){
+			String msg = br.readLine();
+//			Thread.sleep(1000);
+			System.out.println("receive:"+msg);
+			if(i == 2){
+				if(stopWay == SUDDEN_STOP){
+					System.out.println("突然终止程序");
+					System.exit(0);
+				}else if(stopWay == SOCKET_STOP){
+					System.out.println("关闭Socket并终止程序");
+					socket.close();
+					break;
+				}else if(stopWay == INPUT_STOP){
+					System.out.println("关闭输入流并终止程序");
+					socket.shutdownInput();
+					break;
+				}else if(stopWay == SERVERSOCKET_STOP){
+					System.out.println("关闭ServerSocket并终止程序");
+					serverSocket.close();
+				}
+			}
+		}
+		if(stopWay == NUTURAL_STOP){
+			System.out.println("自然关闭..");
+			socket.close();
+			serverSocket.close();
+		}
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		if(args.length > 0) stopWay = Integer.parseInt(args[0]);
+		
+		new Receiver().receive();
+	}
+	
+	
+}
